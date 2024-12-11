3b06296929b5276277ce81f435612a18c3d8c412
Mary-Krause <162741250+Mary-Krause@users.noreply.github.com>
2024-12-11 19:36:37 +0300

    Готово. Домашняя работа по уроку "Цикл for. Элементы списка. Полезные функции в цикле"

--- Modul_2_Operators/module_2_4.py
+++ Modul_2_Operators/module_2_4.py
@@ -1,20 +1,17 @@
-from itertools import count
-
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_Primes = []
-is_prime = 0
-count = 0
+is_prime = True
 for i in numbers:
-    for j in range(2, 15):
-        if i % j == 0:
-            count += 1
-        elif count > 1:
+    if i > 1:
+        for j in range(2, i - 1):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(i)
+        else:
+            is_prime = True
             not_Primes.append(i)
-        else:primes.append(i)
-print(primes)
-print(not_Primes)
-
-   # if i % 2 == 0:
-     #   not_Primes.append(i)
-     #   print(not_Primes)
+print(f'Список с простыми числами: {primes}')
+print(f'Список с сложными числами: {not_Primes}')
