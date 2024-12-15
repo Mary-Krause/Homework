8fbfb9aa6ba06e3d29aa210a3ef716e13a1312af
Mary-Krause <162741250+Mary-Krause@users.noreply.github.com>
2024-12-15 16:35:38 +0300

    Готовая Домашняя работа по уроку "Пространство имён"

---
+++ Modul_3_LearnMoreAboutTheFunctions/module_3_1.py
@@ -0,0 +1,38 @@
+# Переменная для подсчета количества вызовов
+calls = 0
+
+# Функция для подсчета вызовов
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    count = 0
+    for i in string:
+        count += 1
+    max_size = string.upper()
+    min_size = string.lower()
+    string = (count, max_size, min_size)
+    return string
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    strung_up = string.upper()
+    spisok_up = []
+    flag = False
+    for j in list_to_search:
+        spisok_up.append(j.upper())
+    for i in spisok_up:
+        if i == strung_up:
+            flag = True
+    return flag
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'urban', 'urBN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
