f7eb06bf733f7bf4764e52df2638ac1d9c6b3569
Mary-Krause <162741250+Mary-Krause@users.noreply.github.com>
2024-12-15 17:49:05 +0300

    Готовая Домашняя работа по уроку "Способы вызова функции"

--- Modul_3_LearnMoreAboutTheFunctions/module_3_2.py
+++ Modul_3_LearnMoreAboutTheFunctions/module_3_2.py
@@ -1,18 +1,21 @@
-def send_email(message, recipient, *, sender = "university.help@gmail.com"):
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
+    # Проверка корректности адресов
     valid_domains = (".com", ".ru", ".net")
-    if "@" not in recipient or not recipient.endswith(valid_domains) or "@" not in sender or not sender.endswith(valid_domains):
+    if "@" not in recipient or not recipient.endswith(valid_domains) or "@" not in sender or not sender.endswith(
+            valid_domains):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
         return
 
+    # Проверка отправки самому себе
     if sender == recipient:
         print("Нельзя отправить письмо самому себе!")
         return
 
+    # Проверка на отправителя по умолчанию
     if sender == "university.help@gmail.com":
         print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
     else:
         print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
-
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
