d59d6ec80af0f85a1754f08fe2f0ad8db0db990f
Mary-Krause <162741250+Mary-Krause@users.noreply.github.com>
2024-12-24 19:28:03 +0300

    Готовое Дополнительное практическое задание по модулю*

--- Modul_3_LearnMoreAboutTheFunctions/module_3_hard.py
+++ Modul_3_LearnMoreAboutTheFunctions/module_3_hard.py
@@ -1,15 +1,24 @@
-def calculate_structure_sum(*list_data):
+def calculate_structure_sum(data):
+    total = 0
 
+    if isinstance(data, (int, float)):
+        return data
+    elif isinstance(data, str):
+        return len(data)
+    elif isinstance(data, (list, tuple, set)):
+        return sum(calculate_structure_sum(item) for item in data)
+    elif isinstance(data, dict):
+        return sum(calculate_structure_sum(key) + calculate_structure_sum(value) for key, value in data.items())
 
-
+    return total
 
 
 data_structure = [
-[1, 2, 3],
-{'a': 4, 'b': 5},
-(6, {'cube': 7, 'drum': 8}),
-"Hello",
-((), [{(2, 'Urban', ('Urban2', 35))}])
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
 result = calculate_structure_sum(data_structure)
