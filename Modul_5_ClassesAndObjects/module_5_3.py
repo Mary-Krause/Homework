4f8431ce74d807a9efaa5d0af861e70c1959cd18
Mary-Krause <162741250+Mary-Krause@users.noreply.github.com>
2025-02-05 12:30:32 +0300

    Готово Домашняя работа по уроку "Перегрузка операторов"

---
+++ Modul_5_ClassesAndObjects/module_5_3.py
@@ -0,0 +1,90 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        return NotImplemented
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+        return NotImplemented
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+        else:
+            print("Такого этажа не существует")
+
+# Создаем объекты класса House
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+
+# Вызываем метод go_to с различными этажами
+h1.go_to(5)
+h2.go_to(10)
+
+# Примеры работы с перегруженными операторами
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2)
+
+h1 = h1 + 10
+print(h1)
+print(h1 == h2)
+
+h1 += 10
+print(h1)
+
+h2 = 10 + h2
+print(h2)
+
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 != h2)
+
+
+#Теперь класс House поддерживает сравнение по количеству этажей, а также арифметические операции сложения с числами. Вы можете проверить его работу на примерах, приведенных в коде.
