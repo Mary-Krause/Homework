b77d6594ecc7d381575ce10002af36e471f67c56
Mary-Krause <162741250+Mary-Krause@users.noreply.github.com>
2025-02-05 13:07:14 +0300

    Готово Домашнее задание по теме "Позиционирование в файле".

---
+++ Modul_7_WorkingWithFilesAndFormattedOutput/module_7_2.py
@@ -0,0 +1,23 @@
+def custom_write(file_name, strings):
+    strings_positions = {}
+
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for line_number, string in enumerate(strings, start=1):
+            position = file.tell()  # Запоминаем текущую позицию
+            file.write(string + '\n')  # Записываем строку с переходом на новую строку
+            strings_positions[(line_number, position)] = string
+
+    return strings_positions
+
+
+# Пример использования
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
