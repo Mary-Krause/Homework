a6b9d4fda638d216518814ec6c8d4d04a550547e
Mary-Krause <162741250+Mary-Krause@users.noreply.github.com>
2025-02-05 12:33:01 +0300

    Готово Домашняя работа по уроку "Различие атрибутов класса и экземпляра"

--- Modul_5_ClassesAndObjects/module_5_4.py
+++ Modul_5_ClassesAndObjects/module_5_4.py
@@ -1,1 +1,107 @@
+class House:
+    houses_history = []
 
+    def __new__(cls, *args, **kwargs):
+        instance = super().__new__(cls)
+        cls.houses_history.append(args[0])  # Добавляем название дома в историю
+        return instance
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        return NotImplemented
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+        return NotImplemented
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+        else:
+            print("Такого этажа не существует")
+
+# Создаем объекты класса House
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+
+print(House.houses_history)
+
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
+
+# Примеры работы с перегруженными операторами
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2)
+
+h1 = h1 + 10
+print(h1)
+print(h1 == h2)
+
+h1 += 10
+print(h1)
+
+h2 = 10 + h2
+print(h2)
+
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 != h2)
+
+
+#Теперь класс House ведёт историю созданных домов и сообщает об их сносе. Проверьте работу __new__ и __del__, создавая и удаляя объекты!
